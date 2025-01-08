5931de65e0f5c3a556ec98287365271962f6564a
Andrew <sukhanov.a.s22@yandex.ru>
2025-01-08 16:00:51 +0300

    add test of for, while, function

--- parser/test_parser.py
+++ parser/test_parser.py
@@ -2,11 +2,14 @@ from lexer.lexer import Lexer
 from parser import Parser
 
 
-lexer = Lexer(filename='../tests/lexer_tests/test_declarations.pas')
+lexer = Lexer(filename='../tests/lexer_tests/test_programm_block.pas')
 tokens = lexer.tokenize()
 
-for token in tokens:
-    print(token)
+for elem in tokens:
+    print(elem)
+
 parser = Parser(tokens)
+
 ast = parser.parse_program()
 print(ast.children)
+
